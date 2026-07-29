41d94f0fe48fa7dcca35db2760a432669b2a139f
ChiragKumarChouhan <chiragneeraj2@gmail.com>
2026-07-29 14:38:34 +0530

    nn

--- Day1_scoping/global.js
+++
@@ -1,9 +0,0 @@
-// console.log(a);
-// var a=34;{
-//     console.log(a);
-//     a=56;
-//     console.log(a);
-//     a=78
-//     console.log(a);
-// }
-// console.log(a);
--- README.md
+++
@@ -1,2 +0,0 @@
-# GITHUB_REPO_5TH_SEM
-this is a repo for 5th sem FSD
